d0d0d255f0177660de9b7ea2757ed0351790f9b7
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-24 10:32:48 +0530

    added maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,5 +1,32 @@
-def info(message) {
-    echo "INFO: ${message}"
+
+def nodejs(component){
+    sh " echo Installing Jslints"
+    //sh "npm install jslint"
+    //sh "./node_modules/jslint/bin/jslint.js server.js"
+    sh " echo lint checks completed.....!!!!!"
 }
 
-info(message)
+def call()
+{
+pipeline {
+    agent any
+    stages{
+        stage('Lint checks'){
+            steps {
+                script
+                {
+                    nodejs(component)
+                }
+            }
+        }
+
+        stage('Downloading Dependencies'){
+            steps{
+                //sh "npm install"
+                sh "echo npm install"
+            }
+        }
+    }
+}
+}
+
