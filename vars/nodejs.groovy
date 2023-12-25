368709864d19d6ec53044a18bb62f82dcf74e384
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-25 22:44:55 +0530

    Auot push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,10 +5,22 @@ def lintChecks(component){
     sh " echo lint checks completed for ${component}.....!!!!!"
 }
 
+def sonarChecks(component){
+    sh " echo Starting the quality check..."
+    sh " sonar-scanner -Dsonar.host.url=https://172.31.0.188:9000 -Dsonar.source=. -Dsonar.projectKey=${component} -Dsonar.login=${sonar.USR} -Dsonar.password=${sonar.PSW} "
+    //sh "./node_modules/jslint/bin/jslint.js server.js"
+    sh " echo lint checks completed for ${component}.....!!!!!"
+}
+
+
 def call(component)
 {
     pipeline {
         agent any
+
+        environment {
+            sonar = credentials('sonar')
+        }
         stages{
             stage('Lint checks'){
                 steps {
