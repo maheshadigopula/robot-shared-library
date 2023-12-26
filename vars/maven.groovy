0d043d285702fe0a44a06135d1ae712cfb0025e9
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-26 12:11:34 +0530

    Auot push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,6 +5,16 @@ def lintChecks(component){
     sh " echo lint checks completed for ${component}.....!!!!!"
 }
 
+
+def sonarChecks(component){
+    sh " echo Starting the quality check..."
+    sh " sonar-scanner -Dsonar.host.url=http://${sonar_URL}:9000 -Dsonar.source=. -Dsonar.projectKey=catalogue -Dsonar.login=admin -Dsonar.password=123 "
+    sh " curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+    sh " bash -x quality-gate.sh ${sonar_USR} ${sonar_PSW} ${sonar_URL} ${component}"
+    sh " echo lint checks completed for ${component}.....!!!!!"
+}
+
+
 def call(component)
 {
     pipeline {
