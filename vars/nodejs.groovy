1864edfc9ed7278accefd64df642d8cf4233b585
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-29 09:40:28 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,14 +5,6 @@ def lintChecks(component){
     sh " echo lint checks completed for ${component}.....!!!!!"
 }
 
-def sonarChecks(component){
-    sh " echo Starting the quality check..."
-    sh  "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${component}  -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
-    sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
-    sh  "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${component}"
-    sh " echo sonar checks completed for ${component}.....!!!!!"
-}
-
 def call(component)
 {
     pipeline {
@@ -36,6 +28,7 @@ def call(component)
                 steps {
                     script
                     {
+                        env.ARGS="-Dsonar.sources=."
                         sonarChecks(component)
                     }
                 }
